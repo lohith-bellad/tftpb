0df2543061807ae185e926e0c60e4144da40849a
Lohith Bellad <lohith@cloudflare.com>
2019-07-30 15:35:16 -0700

    client side copyright notice add

--- tftpbc.c
+++ tftpbc.c
@@ -1,14 +1,39 @@
-/*----------------------------------------------------------------------------
- Filename:-    tftpbc.c
- Description:- This is the client implementation c file for Trivial File 
-			Transfer Protocol(TFTP)implementation. The implementation 
-			has been done based on RFC 1035, by Sollins, MIT July, 1992.
- Date:- 	     June 30th 2014
- Author:- 	Lohith S Bellad
-               University of Southern California, Information Sciences Institute
- Platform:-    Mac OS X Mountain Lion
- Place:-       Los Angeles, California.
-----------------------------------------------------------------------------*/
+/*
+ * Copyright (c) 2019, Lohith Bellad
+ * Copyright (c) 2014, Lohith Bellad
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *
+ * Client side implementation for Trivial File Transfer Protocol(TFTP).
+ * The implementation is based on RFC 1350, by Sollins, MIT July, 1992.
+ *
+ * June 30th 2014.
+ *
+ * Lohith Bellad
+ * USC Information Sciences Institute, Marina Del Rey, California.
+ */
 
 #include "tftpb.h"
 
